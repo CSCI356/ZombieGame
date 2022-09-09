41e6ff3467d757eec780c3632b6ea5228e0d8f5c
Greg950-glitch <111860224+Greg950-glitch@users.noreply.github.com>
2022-09-09 17:08:04 +1000

    Movement
    Movement

---
+++ Assets/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Touch touch;
+    private float speedModifier = 0.02f;
+    public Vector2 startPos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                //When a touch has first been detected, change the message and record the starting position
+                case TouchPhase.Began:
+                    // Record initial touch position.
+                    startPos = touch.position;
+                    break;
+
+                //Determine if the touch is a moving touch
+                case TouchPhase.Moved:
+                    // Determine direction by comparing the current touch position with the initial one
+                    transform.position = new Vector3(
+                    transform.position.x + touch.deltaPosition.x * speedModifier,
+                    transform.position.y,
+                    transform.position.z + touch.deltaPosition.y * speedModifier);
+                    break;
+
+                case TouchPhase.Ended:
+
+                    break;
+            }
+        }
+
+    }
+}
