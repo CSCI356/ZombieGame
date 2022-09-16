83269909cc4ee13f937f03a74be7c9fef8964dc4
TreeStackH <matthijsuni@gmail.com>
2022-09-17 09:54:20 +1000

    LevelSystem && First radius detection

---
+++ Assets/LevelSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    private int level;
+    private int experience;
+    private int experienceToNextLevel;
+    public float EXPscaling;
+
+    // Start is called before the first frame update
+    public LevelSystem()
+    {
+        level = 0;
+        experience = 0;
+        experienceToNextLevel = 100;
+    }
+
+    public void AddExperience(int amount)
+    {
+        experience += amount;
+        if (experience >= experienceToNextLevel) levelUp();
+        UIManager.Instance.UpdateExperience(experience);
+    }
+
+    private void levelUp()
+    {
+        experience -= experienceToNextLevel;
+        level++;
+        UIManager.Instance.UpdateLevel(level + 1);
+        experienceCalculator();
+    }
+
+    //set level method
+    public void SetLevel(int newLevel)
+    {
+        level = newLevel;
+        experienceCalculator();
+    }
+
+    private void experienceCalculator()
+    {
+        // Calculate exp for next level
+        experienceToNextLevel = (int)(100 * Mathf.Pow(EXPscaling, level));
+
+        // round it down to closest 10
+        experienceToNextLevel -= experienceToNextLevel % 10;
+        //print("experience To Level " + level + " is: " + experienceToNextLevel);
+    }
+}
---
+++ Assets/ZombieTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieTrigger : MonoBehaviour
+{
+    public float radius;
+    private void Start()
+    {
+        transform.localScale = new Vector3(radius, 0.001f, radius);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Zombies")
+        {
+            other.gameObject.GetComponent<ZombieMovement>().inRadius = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Zombies")
+        {
+            GameEvents.current.MovementTrigger();
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Zombies")
+        {
+            other.gameObject.GetComponent<ZombieMovement>().inRadius = false;
+        }
+    }
+
+}
