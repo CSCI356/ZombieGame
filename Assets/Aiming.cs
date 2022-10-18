d3839232daf505aec6ce42f71b470b0c64282cfa
Greg950-glitch <111860224+Greg950-glitch@users.noreply.github.com>
2022-10-18 11:47:22 +1100

    Update and rename Shooting.cs to Aiming.cs

--- Assets/Aiming.cs
+++ Assets/Aiming.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooting : MonoBehaviour
+public class Aiming : MonoBehaviour
 {
     public FixedJoystick Aim;
     Vector3 moveVelocity;
     Vector3 aimVelocity;
     public Rigidbody rb;
-    public GameObject bullet;
-    public Transform firePoint;
-   public float fireSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
     }
 
     // Update is called once per frame
@@ -25,17 +23,7 @@ public class Shooting : MonoBehaviour
         Vector3 AimInput = new Vector3(aimVelocity.x, 0f, aimVelocity.z);
         Vector3 lookAtPoint = transform.position + AimInput;
         transform.LookAt(lookAtPoint);
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
+
 
     }
-    void Shoot()
-    {
-
-        GameObject newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
-        Rigidbody bulletRb = newBullet.GetComponent<Rigidbody>();
-        bulletRb.AddForce(transform.forward * fireSpeed);
-    }
-}
+} 
